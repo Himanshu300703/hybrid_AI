b24f227bce9377f235d2703b81ff57ae6bc0f853
Himanshu300703 <himanshu.44999@gmail.com>
2025-10-19 00:15:49 +0530

    clean config.py

--- config.py
+++ config.py
@@ -2,9 +2,6 @@
 NEO4J_URI = "neo4j://127.0.0.1:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "password"
-
-# OPENAI_API_KEY = "OPENAI_API_KEY" # your OpenAI API key
-
 PINECONE_API_KEY = "PINECONE_API_KEY" # your Pinecone API key
 PINECONE_ENV = "us-east-1"   # example
 PINECONE_INDEX_NAME = "vietnam-travel"
